1385c3f15d857e71b7fbf51ae36cf3b6e6cc4204
Brundi1971 <47036940+Brundi1971@users.noreply.github.com>
2019-01-25 19:59:45 +0200

    main.c

--- Comanda cam/main.c
+++ Comanda cam/main.c
@@ -1,1 +1,42 @@
 //main
+
+#include "config.h"
+#include "uart_lib.h"
+#include "rs232_com.h"
+#include "rs485_com.h"
+#include "read_resolvers.h"
+
+int main(void)
+{   
+   init_porturi_adresa();
+   init_porturi_comenzi_si_senzori();
+   init_resolver_ports();
+   init_remainig_ports();
+   init_timer1();
+   init_UART0();
+   init_UART1();
+   init_LED();
+
+   LED_ON();
+
+   sei();
+
+   if (PIN_PORT_SET_ADRESA & (1 << PIN_ADRESA_0)) ADRESA_MODUL_COMANDA_VIDEO = 0x03;
+   else ADRESA_MODUL_COMANDA_VIDEO = 0x02;
+
+
+   while(1)
+   { 
+     boot_cameras();
+     com_with_master();
+     perform_command();
+	 receive_acknowledge();
+     if (Flag_ActionCommand)
+     {
+       action_command();
+       Flag_ActionCommand = 0;
+     } 
+   }  
+   return(0);   
+}        
+
